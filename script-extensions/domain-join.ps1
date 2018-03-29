930a302fdc41fc6dbd04ac8ae4ed9279a4f8d6cf
Michael Washam <michael@opsgility.com>
2018-03-29 05:24:55 -0500

    updates

---
+++ script-extensions/domain-join.ps1
@@ -0,0 +1,6 @@
+param($domain, $user, $password)
+Start-Sleep -Seconds 120
+$smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+$user = "$domain\$user"
+$objCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $smPassword)
+Add-Computer -DomainName "$domain" -Credential $objCred -Restart -Force
